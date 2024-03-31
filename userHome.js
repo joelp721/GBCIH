45d2dc4091c7e72a59f02c4ac75b6ef10be92891
Dumke01 <158321003+Dumke01@users.noreply.github.com>
2024-03-30 22:27:20 -0500

    Created the calendar, started on CSS

--- userHome.js
+++ userHome.js
@@ -5,13 +5,39 @@ let clicked = null;
 let events= localStorage.getItem('events') ? JSON.parse(localStorage.getItem('events')) : [];
 
 const calendar = document.getElementById('calendar');
+const newEventModal = document.getElementById('newEventModal');
+const deleteEventModal = document.getElementById('deleteEventModal');
+const backDrop = document.getElementById('modalBackDrop');
+const eventTitleInput = document.getElementById('eventTitleInput');
 const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
+function openModal(date)
+{
+    clicked=date;
+
+    const eventForDay = events.find(e => e.date === clicked);
+    if(eventForDay)
+    {
+        // console.log("Event exists");
+        document.getElementById('eventText').innerText = eventForDay.title;
+        deleteEventModal.style.display='block';
+    }
+    else
+    {
+        newEventModal.style.display = 'block';
+    }
+    
+    backDrop.style.display = 'block';
+}
 
 function load()
 {
     const dt = new Date();
 
+    if(nav !== 0)
+    {
+        dt.setMonth(new Date().getMonth() + nav);
+    }
     // console.log(dt);
 
     const day=dt.getDate();
@@ -22,7 +48,9 @@ function load()
 
     const firstDayOfMonth= new Date(year, month, 1);
     //this will give the number of days in the month
-    const daysInMonth=new Date(year, month + 1, 0).getDate();
+    const lastDayOfMonth = new Date(year, month + 1 ,0);
+    const daysInMonth = lastDayOfMonth.getDate();
+    // const daysInMonth=new Date(year, month + 1, 0).getDate();
     // console.log(daysInMonth);
 
     const dateString=firstDayOfMonth.toLocaleDateString('en-us', {
@@ -36,16 +64,37 @@ function load()
     const paddingDays=weekdays.indexOf(dateString.split(', ')[0]);
     // console.log(paddingDays);
 
+    document.getElementById('monthDisplay').innerText =`${dt.toLocaleDateString('en-us',{month: 'long'})} ${year}`
+
+    calendar.innerHTML = '';    //rerenders the calendar to prevent duplicate calendars
+   
     for(let i=1; i<=paddingDays+daysInMonth; ++i)
     {
         const daySquare=document.createElement('div');
         daySquare.classList.add('day');
 
+        const dayString = `${month+1}/${i-paddingDays}/${year}`;
+
         if(i>paddingDays)
         {
             daySquare.innerText=i-paddingDays;
 
-            daySquare.addEventListener('click', ()=> console.log('click'));
+            const eventForDay= events.find(e => e.date === dayString) 
+
+            if(i - paddingDays===day&&nav===0)
+            {
+                daySquare.id='currentDay';
+                
+            }
+            if(eventForDay)
+            {
+                const eventDiv = document.createElement('div');
+                eventDiv.classList.add('event');
+                eventDiv.innerText=eventForDay.title;
+                daySquare.appendChild(eventDiv);
+            }
+
+            daySquare.addEventListener('click', () => openModal(dayString));
         }
         else
         {
@@ -56,4 +105,64 @@ function load()
     }
 }
 
+function closeModal()
+{
+    eventTitleInput.classList.remove('error');
+    newEventModal.style.display='none';
+    deleteEventModal.style.display='none';
+    backDrop.style.display='none';
+    eventTitleInput.value='';
+    clicked=null;
+    load();
+
+}
+
+function saveEvent()
+{   
+    if(eventTitleInput.value)
+    {
+        eventTitleInput.classList.remove('error');
+
+        events.push({
+            date: clicked,
+            title: eventTitleInput.value,
+        });
+        
+        //save to local storage
+        localStorage.setItem('events', JSON.stringify(events));
+        closeModal();
+    }
+    else
+    {
+        eventTitleInput.classList.add('error');
+    }
+}
+
+function deleteEvent()
+{
+    events= events.filter(e => e.date !== clicked);
+    localStorage.setItem('events', JSON.stringify(events));
+    closeModal();
+}
+
+function initButtons()
+{
+    document.getElementById('nextButton').addEventListener('click', () => {
+        nav++;
+        load();
+    });
+    document.getElementById('backButton').addEventListener('click', () => {
+        nav--;
+        load();
+    });
+
+    document.getElementById('saveButton').addEventListener('click', saveEvent);
+    document.getElementById('cancelButton').addEventListener('click', closeModal);
+
+    document.getElementById('deleteButton').addEventListener('click', deleteEvent);
+    document.getElementById('closeButton').addEventListener('click', closeModal);
+
+}
+
+initButtons();
 load();
